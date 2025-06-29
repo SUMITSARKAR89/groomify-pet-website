3299bcfc074c958db67114bc34a2e02fbd409215
Sumit-sarkar <sumitopticalit89@gmail.com>
2025-06-30 02:00:33 +0600

    50% processing

--- asset/Jsfile/app.js
+++ asset/Jsfile/app.js
@@ -12,3 +12,24 @@ closeIcon.addEventListener("click", () => {
      
     menuIcon.style.display = "block";
 });
+
+
+
+const tabBtn = document.querySelectorAll(".tab-btn");
+const tabContent = document.querySelectorAll(".tab-content");
+
+tabBtn.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        tabBtn.forEach( btn => {
+            btn.classList.remove("active")
+        })
+        btn.classList.add("active");
+        
+
+        tabContent.forEach(content => {
+            content.classList.remove('tabActive');
+
+        })
+        tabContent[i].classList.add("tabActive")
+    })
+})
